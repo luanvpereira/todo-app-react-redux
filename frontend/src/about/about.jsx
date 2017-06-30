32e1d917ed90dfd05d3630e6c68258a551d7b926
= <=>
2017-06-30 15:16:01 -0300

    Add todo-app-react

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,18 @@
+import	React	from	'react';
+
+import	PageHeader	from	'../templates/page-header';
+
+export	default	props	=>	(
+	<div>
+		<PageHeader name="About" small="Us"/>
+
+		<h2>Our story</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro unde saepe, vero, repellat animi voluptates eius blanditiis quam pariatur esse corporis necessitatibus eum quasi quos veritatis excepturi aliquam ad laudantium.</p>
+
+		<h2>See our Mission and Vision</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro unde saepe, vero, repellat animi voluptates eius blanditiis quam pariatur esse corporis necessitatibus eum quasi quos veritatis excepturi aliquam ad laudantium.</p>
+
+		<h2>Press</h2>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro unde saepe, vero, repellat animi voluptates eius blanditiis quam pariatur esse corporis necessitatibus eum quasi quos veritatis excepturi aliquam ad laudantium.</p>
+	</div>
+)
